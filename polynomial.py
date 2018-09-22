a8ccbb240c9cb32819d49899424840c053fbe8b6
Kevin pedroza <kevin.andres01@hotmail.com>
2018-09-22 11:18:51 -0500

    Finished. It is used tabulate module to show table

--- polynomial.py
+++ polynomial.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 18 09:31:36 2018
-
-@author: ceisutb17
-"""
 import numpy as np
 
 
-def polynomial_feature(grade):
-    dataSet = np.loadtxt("ex2data1.txt",delimiter=',')
-    X = dataSet[:,0]
-    
-    changes = np.zeros(X.shape)
-    
-    #changes = []
-    #y=dataSet[:,1]
-    if grade >= 2:
-        for i in range(1, grade):
-            aux = False
+def polynomial_feature(degree):
+    dataSet = np.loadtxt("ex2data1.txt", delimiter=',')
+
+    sixty = int(dataSet.shape[0] * 0.6)
+
+    X = dataSet[:, 0]
+    changes = []
+
+    if degree >= 2:
+        for i in range(1, degree):
             aux = np.array(0)
             for value in X:
-                aux = np.append(aux, value**(i+1))
+                aux = np.append(aux, np.power(value, i+1))
             aux = np.delete(aux, 0)
-            print("aux",aux.shape)
-            X = np.c_[X, aux]
-            print("x", X.shape)
-    elif grade == 1:
-        pass
-        #return X
-    else:
-        pass
-        #return "Error"
-    
-    print(X)
+            changes.append(aux)
+        for i in changes:
+            X = np.c_[X, i]
+    X = np.c_[np.ones(dataSet.shape[0]), X]
 
+    x_train = X[:sixty]
+    x_test = X[sixty:]
 
-if __name__ == '__main__':
-    polynomial_feature(3)
+    y_train = dataSet[:sixty, 1]
+    y_test = dataSet[sixty:, 1]
+
+    return x_train, x_test, y_train, y_test
--- template1.py
+++ template1.py
@@ -1,14 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-dataSet=np.loadtxt("ex2data1.txt",delimiter=',')
-X=dataSet[:,0]
-y=dataSet[:,1]
-
-print(X)
+from polynomial import polynomial_feature as pf
+from tabulate import tabulate
 
 
-#Plotting data
-plt.plot(X, y, "b.")
-plt.axis([-3, 3, 0, 10])
-plt.show()
+def mape(theta, data_x, data_y):
+    val = 0
+    for i in range(data_y.shape[0]):
+        hx = 0
+        for j in range(theta.shape[0]):
+            hx += (theta[j] * data_x[i][j])
+        val += (abs(data_y[i] - hx) / data_y[i])
+    val = (val / data_y.shape[0]) * 100
+
+    #print("MAPE es:", val, "% de error.")
+    return val
+    
+
+def regression(x, y):
+    X = np.matrix(x)
+    Y = np.matrix(y)
+
+    theta = X.T * X
+    theta = theta.I
+    theta = theta * X.T
+    theta = theta * Y.T
+
+    theta = np.array(np.array(theta.T)[0])
+
+    return theta
+
+
+def get_values():
+    dataSet = np.loadtxt("ex2data1.txt", delimiter=',')
+    # X = dataSet[:, 0]
+    y = dataSet[:, 1]
+    return y
+
+
+def show_vector(vector):
+    for j in range(vector.shape[0]):
+        if j != 0:
+            print(str(vector[j]) + 'x' + str(j), end=' ')
+        else:
+            print(str(vector[j]), end=' ')
+        if j+1 != vector.shape[0]:
+            print('+', end=' ')
+
+
+def main():
+
+    tabla = []
+
+    for i in range(1, 20):
+        tab_aux = []
+        #print("\nGRADO", i)
+        tab_aux.append(i)
+        x1, x2, y1, y2 = pf(i)
+        vector = regression(x1, y1)
+        # show_vector(vector)
+        #print("\nANALISIS DE h(x) GRADO ", i)
+        tab_aux.append(mape(vector, x1, y1))
+        tab_aux.append(mape(vector, x2, y2))
+        tabla.append(tab_aux)
+    print(tabulate(tabla, headers=['Grado del polinomio', 'MAPE con train set (%)', 'MAPE con test set (%)']))
+
+
+def plot(X, y):
+    #Plotting data
+    plt.plot(X, y, "b.")
+    plt.axis([-3, 3, 0, 10])
+    plt.show()
+
+main()
