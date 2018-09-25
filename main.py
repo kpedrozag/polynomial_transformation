8961e7c147f4e60629530cdba303f2e2d20cf49f
kpedrozag <32544147+kpedrozag@users.noreply.github.com>
2018-09-24 22:04:36 -0500

    comments added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from polynomial import polynomial_feature as pf
 from tabulate import tabulate
 from MyFunctions import *
 
+# funcion principal
 def main():
 
     tabla = []
+    # se pide el grado hasta el cual se quieren hacer las transformaciones
     while True:
         try:
             n = int(input("Digite el grado de la transformacion polinomial deseada: "))
@@ -13,30 +15,32 @@ def main():
             break
         except Exception:
             print("Valor inválido")            
-    
+    # para cada grado indicado, se hace lo siguiente
     for i in range(1, n+1):
+        # lista auxiliar para guardar info para tabular
         tab_aux = []
         print("-"*50)
         print("\nGRADO", i)
         tab_aux.append(i)
+        # se calculan las 4 matrices con las transformaciones de grado 'i'
         x1, x2, y1, y2 = pf(i)
+        # se calculan las thetas del grado 'i'
         vector = regression(x1, y1)
-        # show_vector(vector)
-        #print("\nANALISIS DE h(x) GRADO ", i)
+        # se calcula el Error Cuadrático Medio para el conjunto de entrenamiento 
+        # y para el conjunto de pruebas
         val1, hx1 = mse(vector, x1, y1)
         val2, hx2 = mse(vector, x2, y2)
         tab_aux.append(val1)
         tab_aux.append(val2)
-        
+        # se preparan datos para hacer la grafica
         x1 = x1[:, 1:]
         x2 = x2[:, 1:]
-        
-        #print(test)
-        
+        # se hacen las graficas de cada transformacion        
         plot(x1, hx1)
         plot(x2, hx2)
         
         tabla.append(tab_aux)
+    # se imprime una tabla que resume los errores cuadráticos medios de cada regresion
     print(tabulate(tabla, headers=['Grado del polinomio', 'MSE con train set', 'MSE con test set']))
 
 main()
