82357aa7da2e527e3644ed619f4620067d2b6c49
ceisutb17 <ceisutb17@iMac-de-ceisutb17.local>
2018-09-25 10:22:10 -0500

    Plot of MSE

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from polynomial import polynomial_feature as pf
 from tabulate import tabulate
 from MyFunctions import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 # funcion principal
 def main():
@@ -14,13 +16,14 @@ def main():
                 raise RuntimeError
             break
         except Exception:
-            print("Valor inválido")            
+            print("Valor inválido")   
+    print("\n\n")
     # para cada grado indicado, se hace lo siguiente
     for i in range(1, n+1):
         # lista auxiliar para guardar info para tabular
         tab_aux = []
-        print("-"*50)
-        print("\nGRADO", i)
+        # print("-"*50)
+        # print("\nGRADO", i)
         tab_aux.append(i)
         # se calculan las 4 matrices con las transformaciones de grado 'i'
         x1, x2, y1, y2 = pf(i)
@@ -35,12 +38,29 @@ def main():
         # se preparan datos para hacer la grafica
         x1 = x1[:, 1:]
         x2 = x2[:, 1:]
-        # se hacen las graficas de cada transformacion        
-        plot(x1, hx1)
-        plot(x2, hx2)
+        # se hacen las graficas de cada transformacion
+        # plot(x1, hx1)
+        # plot(x2, hx2)
         
         tabla.append(tab_aux)
     # se imprime una tabla que resume los errores cuadráticos medios de cada regresion
-    print(tabulate(tabla, headers=['Grado del polinomio', 'MSE con train set', 'MSE con test set']))
+    
+    print(tabulate(tabla, headers=['Degree of the polynomial', 'MSE with train set', 'MSE with test set']))
+    
+    graf = np.array(tabla)
+    
+    grado = graf[:, 0]
+    error1 = graf[:, 1]
+    error2 = graf[:, 2]
+    
+    plt.plot(grado, error1, 'bo-', label='MSE with train dataset')
+    plt.plot(grado, error2, 'ro--', label='MSE with test dataset')
+    plt.axis([0, 25, 0.0, 12.0])
+    plt.title('Comparison among MSE of train dataset and test dataset')
+    plt.xlabel('Degree of the polynomial')
+    plt.ylabel('MSE')
+    plt.legend(loc=2)
+    plt.show()
+    
 
 main()
